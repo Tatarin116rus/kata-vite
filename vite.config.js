cd7c612e9593b3abf80e4ebe53021025ccd659c7
Tatarin116rus <youngerxabi.aidar@gmail.com>
2026-01-15 23:10:34 +0300

    vitets

---
+++ vite.config.js
@@ -0,0 +1,54 @@
+import { defineConfig } from "vitest/config";
+import react from '@vitejs/plugin-react'
+import path from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+    test: {
+globals: true,
+environment: "jsdom",
+},
+  base: '/kata-vite/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  css: {
+    modules: {
+      localsConvention: 'camelCase',
+      generateScopedName: '[name]__[local]___[hash:base64:5]'
+    },
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/styles/variables.scss";`
+      }
+    }
+  },
+  server: {
+    port: 3000,
+    open: true
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true,
+     rollupOptions: {
+    output: {
+
+      assetFileNames: (assetInfo) => {
+        let extType = assetInfo.name.split('.').at(1);
+        if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+          extType = 'images';
+        }
+       
+        return `assets/${extType}/[name]-[hash][extname]`;
+      },
+     
+      chunkFileNames: 'assets/js/[name]-[hash].js',
+      entryFileNames: 'assets/js/[name]-[hash].js',
+    }
+    }
+  }
+
+});
